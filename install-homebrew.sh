84d7aa9fb2d70d99085f299807ffc731bea6267f
Milan Aleks <42142708+milanmayr@users.noreply.github.com>
2022-08-31 14:26:28 -0500

    Update install-homebrew.sh

--- install-homebrew.sh
+++ install-homebrew.sh
@@ -1,1 +1,6 @@
+# Run Homebrew install script
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add Homebrew to PATH
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
